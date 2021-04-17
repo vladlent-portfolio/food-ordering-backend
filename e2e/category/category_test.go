5a2a01718bf4f30e16a266234f0cf466f4c5f3af
Vladlen Tereshchenko <vladlent.dev@gmail.com>
2021-04-17 12:38:51 +0300

    (category): refactored e2e tests

--- e2e/category/category_test.go
+++ e2e/category/category_test.go
@@ -21,24 +21,14 @@ var db = database.MustGetTest()
 var r = router.Setup(db)
 
 func TestCategories(t *testing.T) {
-	cleanup()
-	t.Cleanup(cleanup)
 	db.Logger.LogMode(logger.Silent)
 
 	t.Run("GET /categories", func(t *testing.T) {
 		send := sendReq(http.MethodGet, "/categories")
+
 		t.Run("should return JSON array of categories", func(t *testing.T) {
-			t.Cleanup(cleanup)
+			setupDB(t)
 			it := assert.New(t)
-			var categories []category.Category
-
-			db.Find(&categories)
-			require.Len(t, categories, 0)
-
-			db.Create(&testCategories)
-			db.Find(&categories)
-			require.Len(t, categories, len(testCategories))
-
 			resp := send("")
 
 			it.Equal(http.StatusOK, resp.Code)
@@ -51,23 +41,16 @@ func TestCategories(t *testing.T) {
 	})
 
 	t.Run("GET /categories/:id", func(t *testing.T) {
-		t.Cleanup(cleanup)
 		sendWithParam := func(id uint) *httptest.ResponseRecorder {
 			param := strconv.Itoa(int(id))
 			return sendReq(http.MethodGet, "/categories/"+param)("")
 		}
 
 		t.Run("should return category with provided id", func(t *testing.T) {
-			t.Cleanup(cleanup)
-			cleanup()
+			setupDB(t)
 			it := assert.New(t)
 
-			db.Create(&testCategories)
-			var categories []category.Category
-			db.Find(&categories)
-			require.Len(t, categories, len(testCategories))
-
-			for _, cat := range categories {
+			for _, cat := range testCategories {
 				resp := sendWithParam(cat.ID)
 				it.Equal(http.StatusOK, resp.Code)
 				it.Equal(
@@ -82,17 +65,11 @@ func TestCategories(t *testing.T) {
 	})
 
 	t.Run("POST /categories", func(t *testing.T) {
-		t.Cleanup(cleanup)
 		send := sendReq(http.MethodPost, "/categories")
 
 		t.Run("should add category to db and return it", func(t *testing.T) {
-			cleanup()
-			t.Cleanup(cleanup)
+			setupDB(t)
 			it := assert.New(t)
-
-			var categories []category.Category
-			db.Find(&categories)
-			initialLen := len(categories)
 			json := `{"id":69,"title":"Pizza","removable":false}`
 
 			resp := send(json)
@@ -108,13 +85,13 @@ func TestCategories(t *testing.T) {
 			it.Equal(last.Title, "Pizza")
 			it.Equal(last.ID, uint(69))
 
-			db.Find(&categories)
-			it.Len(categories, initialLen+1)
+			var categories []category.Category
+			if it.NoError(db.Find(&categories).Error) {
+				it.Len(categories, len(testCategories)+1)
+			}
 		})
 
 		t.Run("should return 400 if provided json isn't correct", func(t *testing.T) {
-			cleanup()
-			t.Cleanup(cleanup)
 			it := assert.New(t)
 			json := `{"title": 123}`
 
@@ -123,10 +100,9 @@ func TestCategories(t *testing.T) {
 		})
 
 		t.Run("should return 409 if category already exists", func(t *testing.T) {
-			cleanup()
-			t.Cleanup(cleanup)
+			setupDB(t)
 			it := assert.New(t)
-			json := `{"id":1,"title":"Salads","removable":false}`
+			json := `{"id":13,"title":"Salads","removable":false}`
 
 			resp := send(json)
 			it.Equal(http.StatusCreated, resp.Code)
@@ -137,93 +113,52 @@ func TestCategories(t *testing.T) {
 	})
 
 	t.Run("PUT /categories/:id", func(t *testing.T) {
-		t.Cleanup(cleanup)
 		sendWithParam := func(id uint, body string) *httptest.ResponseRecorder {
 			param := strconv.Itoa(int(id))
 			return sendReq(http.MethodPut, "/categories/"+param)(body)
 		}
 
-		setup := func(t *testing.T, initial category.Category) category.Category {
-			var c category.Category
-			db.Create(&initial)
-			db.First(&c, initial.ID)
-			require.Equal(t, c.ID, initial.ID)
-			return c
-		}
-
 		t.Run("should update category in db based on provided json", func(t *testing.T) {
-			cleanup()
-			t.Cleanup(cleanup)
+			setupDB(t)
 			it := assert.New(t)
 			testCategory := testCategories[0]
 			updateJSON := `{"title":"Sushi","removable":true}`
 
-			c := setup(t, testCategory)
-
-			resp := sendWithParam(c.ID, updateJSON)
+			resp := sendWithParam(testCategory.ID, updateJSON)
 			it.Equal(http.StatusOK, resp.Code)
 			it.Equal(fmt.Sprintf(`{"id":%d,"title":"Sushi","removable":true}`, testCategory.ID), resp.Body.String())
 		})
 
 		t.Run("should ignore id in provided json", func(t *testing.T) {
-			cleanup()
-			t.Cleanup(cleanup)
+			setupDB(t)
 			it := assert.New(t)
 			testCategory := testCategories[0]
 			updateJSON := `{"id":420,"title":"Sushi","removable":true}`
 			require.NotEqual(t, testCategory.ID, 420)
 
-			c := setup(t, testCategory)
-
-			resp := sendWithParam(c.ID, updateJSON)
+			resp := sendWithParam(testCategory.ID, updateJSON)
 			it.Equal(http.StatusOK, resp.Code)
 			it.Equal(fmt.Sprintf(`{"id":%d,"title":"Sushi","removable":true}`, testCategory.ID), resp.Body.String())
 		})
-
-		// Leave this for PATCH request
-		//t.Run("should only update title", func(t *testing.T) {
-		//	cleanup()
-		//	it := assert.New(t)
-		//	testCategory := testCategories[0]
-		//	updateJSON := `{"title":"Sushi"}`
-		//	expected := `{"id":1,"title":"Sushi","removable":true}`
-		//
-		//	testCategory.Removable = true
-		//
-		//	c := setup(t, testCategory)
-		//	resp := sendWithParam(c.ID, updateJSON)
-		//	it.Equal(http.StatusOK, resp.Code)
-		//	it.Equal(expected, resp.Body.String())
-		//})
 
 		runFindByIDTests(t)
 	})
 
 	t.Run("DELETE /categories/:id", func(t *testing.T) {
-		t.Cleanup(cleanup)
 		sendWithParam := func(id uint) *httptest.ResponseRecorder {
 			param := strconv.Itoa(int(id))
 			return sendReq(http.MethodDelete, "/categories/"+param)("")
 		}
 
 		t.Run("should removed a category with provided ID from db", func(t *testing.T) {
-			t.Cleanup(cleanup)
-			cleanup()
+			setupDB(t)
 			it := assert.New(t)
-			var categories []category.Category
-
-			db.Find(&categories)
-			require.Len(t, categories, 0)
-
-			db.Create(&testCategories)
-			db.Find(&categories)
-			require.Len(t, categories, len(testCategories))
-
 			testCat := testCategories[len(testCategories)/2]
 			resp := sendWithParam(testCat.ID)
 
 			it.Equal(http.StatusOK, resp.Code)
 
+			var categories []category.Category
 			db.Find(&categories)
 			it.Len(categories, len(testCategories)-1)
 
@@ -233,8 +168,7 @@ func TestCategories(t *testing.T) {
 		})
 
 		t.Run("should return 403 if category isn't removable", func(t *testing.T) {
-			t.Cleanup(cleanup)
-			cleanup()
+			setupDB(t)
 			it := assert.New(t)
 			c := category.Category{Title: "Fish", Removable: false}
 
@@ -248,22 +182,22 @@ func TestCategories(t *testing.T) {
 
 func runFindByIDTests(t *testing.T) {
 	t.Run("should return 400 if provided id isn't valid", func(t *testing.T) {
-		t.Cleanup(cleanup)
-		cleanup()
+		setupDB(t)
 		resp := sendReq(http.MethodGet, "/categories/some-random-id")("")
 		assert.Equal(t, http.StatusBadRequest, resp.Code)
 	})
 
 	t.Run("should return 404 if category with provided id doesn't exist", func(t *testing.T) {
-		t.Cleanup(cleanup)
-		cleanup()
-		var categories []category.Category
-		db.Find(&categories)
-		require.Len(t, categories, 0)
-
+		setupDB(t)
 		resp := sendReq(http.MethodGet, "/categories/69")("")
 		assert.Equal(t, http.StatusNotFound, resp.Code)
 	})
+}
+
+func setupDB(t *testing.T) {
+	cleanup()
+	t.Cleanup(cleanup)
+	db.Create(&testCategories)
 }
 
 func sendReq(method, target string) func(body string) *httptest.ResponseRecorder {

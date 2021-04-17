bea2dd22e86c8cadf7a773c17957c705579e8d14
Vladlen Tereshchenko <vladlent.dev@gmail.com>
2021-04-17 14:22:33 +0300

    (test): fixed test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,6 @@
-cd ../controllers || exit 1
+root=$(dirname "$(dirname "$(realpath "$0")")")
+cd "$root" || exit 1
+cd ./controllers || exit 1
 go test ./...
 cd ../e2e || exit 1
 # disable parallel test execution

ba52970724f7ecbaf0260dea3b7a2b5fc372bf8d
Vladlen Tereshchenko <vladlent.dev@gmail.com>
2021-05-12 15:21:56 +0300

    (router): adds "Access-Control-Allow-Headers" header to CORSMiddleware

--- router/router.go
+++ router/router.go
@@ -38,6 +38,7 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "http://localhost:4200")
 		c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "*")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)

9e0660440a3ca2c5f7772c58e9caddd1dfe6c77d
Vladlen Tereshchenko <vladlent.dev@gmail.com>
2021-05-18 17:54:42 +0300

    (dish): refactors API to use upload service

--- controllers/dish/api.go
+++ controllers/dish/api.go
@@ -1,15 +1,13 @@
 package dish
 
 import (
-	"fmt"
 	"food_ordering_backend/common"
 	"food_ordering_backend/config"
 	"food_ordering_backend/controllers/user"
+	"food_ordering_backend/services"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"net/http"
-	"os"
-	"path"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -17,10 +15,17 @@ import (
 
 type API struct {
 	service *Service
+	upload  *services.Upload
 }
 
 func ProvideAPI(s *Service) *API {
-	return &API{s}
+	upload := &services.Upload{
+		AllowedTypes: []string{"image/png", "image/jpeg"},
+		MaxFileSize:  config.MaxUploadFileSize,
+		Root:         config.DishesImgDirAbs,
+		FormDataKey:  "image",
+	}
+	return &API{s, upload}
 }
 
 func (api *API) Register(router *gin.RouterGroup, db *gorm.DB) {
@@ -173,52 +178,14 @@ func (api *API) Upload(c *gin.Context) {
 		return
 	}
 
-	fileHeader, err := c.FormFile("image")
+	absPath := api.upload.ParseAndSave(c, strconv.Itoa(int(dish.ID)))
 
-	if err != nil {
-		c.String(http.StatusBadRequest, err.Error())
+	if absPath == "" {
 		return
 	}
 
-	if fileHeader.Size > config.MaxUploadFileSize {
-		c.Status(http.StatusRequestEntityTooLarge)
-		return
-	}
-
-	file, err := fileHeader.Open()
-
-	if err != nil {
-		c.Status(http.StatusInternalServerError)
-		return
-	}
-
-	defer file.Close()
-
-	contentType, err := common.MIMEType(file)
-
-	if err != nil {
-		c.Status(http.StatusUnprocessableEntity)
-		return
-	}
-
-	if contentType != "image/png" && contentType != "image/jpeg" {
-		c.Status(http.StatusUnsupportedMediaType)
-		return
-	}
-
-	fPath := filepath.Join(config.DishesImgDirAbs, fmt.Sprintf("%d.%s", dish.ID, path.Base(contentType)))
-	if err := os.MkdirAll(filepath.Dir(fPath), os.ModeDir); err != nil {
-		c.Status(http.StatusInternalServerError)
-		return
-	}
-
-	if err := c.SaveUploadedFile(fileHeader, fPath); err != nil {
-		c.Status(http.StatusInternalServerError)
-		return
-	}
-
-	fName := filepath.Base(fPath)
-	dish.Image = &fName
+	name := filepath.Base(absPath)
+	dish.Image = &name
 
 	dish, err = api.service.Save(dish)
 
@@ -227,7 +194,7 @@ func (api *API) Upload(c *gin.Context) {
 		return
 	}
 
-	c.String(http.StatusOK, PathToImg(fName))
+	c.String(http.StatusOK, PathToImg(name))
 }
 
 // Delete godoc

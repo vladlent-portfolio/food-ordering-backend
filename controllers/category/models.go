a086b049c7c15fb546d2fcf893a085ed866bf175
Vladlen Tereshchenko <vladlent.dev@gmail.com>
2021-06-23 14:07:15 +0300

    (category): removes default value for Category.Removable

--- controllers/category/models.go
+++ controllers/category/models.go
@@ -3,6 +3,6 @@ package category
 type Category struct {
 	ID        uint   `gorm:"primaryKey"`
 	Title     string `gorm:"size:255;unique;not null"`
-	Removable bool   `gorm:"default:true"`
+	Removable bool
 	Image     *string
 }

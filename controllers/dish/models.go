adcc274788e9ca4f346d69144d7c7f6e41d936b6
Vladlen Tereshchenko <vladlent.dev@gmail.com>
2021-07-14 16:13:22 +0300

    (dish): moves image deletion to 'AfterDelete' hook

--- controllers/dish/models.go
+++ controllers/dish/models.go
@@ -1,7 +1,12 @@
 package dish
 
 import (
+	"food_ordering_backend/config"
 	"food_ordering_backend/controllers/category"
+	"gorm.io/gorm"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 type Dishes []Dish
@@ -14,6 +19,16 @@ type Dish struct {
 	Removable  bool `gorm:"default:true"`
 	CategoryID uint
 	Category   category.Category `gorm:"constraint:OnDelete:CASCADE,OnUpdate:CASCADE"`
+}
+
+func (d *Dish) AfterDelete(tx *gorm.DB) (err error) {
+	if d.Image != nil {
+		err = os.Remove(filepath.Join(config.DishesImgDirAbs, *d.Image))
+		if err != nil {
+			log.Println("[Dish] Error deleting image:", err)
+		}
+	}
+	return
 }
 
 func (dishes Dishes) Find(lookup func(d Dish, index int) bool) (Dish, bool) {
